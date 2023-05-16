08ff070bc7fd32cc5795201487fd553db6f1cd9b
Volodymyr Pivtoranis <pivtoranis87@gmail.com>
2023-05-16 23:24:35 +0400

    Added static Home Page content

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,23 @@
-const page = () => {
-  return <div>Home Page</div>;
+const Home = () => {
+  return (
+    <section className="w-full">
+      <h1 className="mt-5 text-4xl font-extrabold leading-[1.15] text-black sm:text-6xl text-center">
+        Share & Explore
+        <br />
+        <span className="bg-gradient-to-r from-blue-500 to-yellow-400 bg-clip-text text-transparent text-center">
+          Welcome to TripSnap!
+        </span>
+      </h1>
+      <p className="mt-5 text-lg text-gray-500 sm:text-xl lg:text-2xl max-w-2xl text-center">
+        It&apos;s the perfect place to share and discover amazing travel
+        experiences from all over the world. Whether you&apos;re a frequent
+        traveler or just love to read about travel, TripSnap lets you create
+        your own travel story. Share your exciting adventures, from thrilling
+        trips to peaceful getaways, and inspire others to go on their own
+        unforgettable journeys.
+      </p>
+    </section>
+  );
 };
 
-export default page;
+export default Home;
