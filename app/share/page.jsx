5d630feaa869614b61f762de7d63c8cb941985b3
Volodymyr Pivtoranis <pivtoranis87@gmail.com>
2023-05-20 15:13:30 +0400

    Added static text to share page

---
+++ app/share/page.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const NewTrip = () => {
+  return (
+    <section className="flex flex-col">
+      <h1 className="mt-5 text-4xl font-extrabold leading-[1.15] text-black sm:text-6xl text-center">
+        <span className="bg-gradient-to-r from-blue-500 to-yellow-400 bg-clip-text text-transparent">
+          Share Your Trip
+        </span>
+      </h1>
+      <p className="mt-5 text-lg text-gray-500 sm:text-xl lg:text-2xl max-w-2xl text-center">
+        Traveling is all about discovering amazing places, breathtaking views,
+        and exciting adventures. We want to hear about your trip and inspire
+        other adventurers to embark on their own extraordinary journeys. <br />
+        Whether you found hidden gems, explored stunning landscapes, or had
+        thrilling experiences, share the details with us. Your story can ignite
+        the wanderlust in others and encourage them to create their own
+        unforgettable memories.
+      </p>
+    </section>
+  );
+};
+
+export default NewTrip;
