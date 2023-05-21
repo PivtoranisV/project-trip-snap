91bbc53640ee61e548199757ab6389516ee278a4
Volodymyr Pivtoranis <pivtoranis87@gmail.com>
2023-05-21 22:16:24 +0400

    Added module for Trip

---
+++ models/trip.js
@@ -0,0 +1,32 @@
+import { Schema, models, model } from 'mongoose';
+
+const TripSchema = new Schema({
+  traveler: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+  },
+  title: {
+    type: String,
+    required: [true, 'Title is required'],
+  },
+  country: {
+    type: String,
+    required: [true, 'Country is required'],
+  },
+  date: {
+    type: Date,
+    required: [true, 'Date is required'],
+  },
+  hotel: {
+    type: String,
+    required: [true, 'Hotel is required'],
+  },
+  details: {
+    type: String,
+    required: [true, 'Details are required'],
+  },
+});
+
+const Trip = models.Trip || model('Trip', TripSchema);
+
+export default Trip;
