9358662ec1b44a62dbdb8d53b788e688f5c19f40
Volodymyr Pivtoranis <pivtoranis87@gmail.com>
2023-05-24 11:22:30 +0400

    Added search component

--- components/Search.jsx
+++ components/Search.jsx
@@ -1,7 +1,23 @@
-import React from 'react';
+'use client';
+import { useState } from 'react';
 
 const Search = () => {
-  return <div>Search</div>;
+  const [searchText, setSearchText] = useState('');
+  const handleSearchChange = (event) => {
+    setSearchText(event.target.value);
+  };
+  return (
+    <form className="w-full">
+      <input
+        type="text"
+        placeholder="Search for a Country or Username"
+        value={searchText}
+        required
+        onChange={handleSearchChange}
+        className="block w-full rounded-md border border-gray-300 bg-white/80 py-2.5 font-satoshi pl-5 pr-12 text-sm shadow-lg font-medium focus:border-black focus:outline-none"
+      />
+    </form>
+  );
 };
 
 export default Search;
