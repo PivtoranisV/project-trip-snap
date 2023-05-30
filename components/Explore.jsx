2ff2c4ee16a5a88936746d13aa374b9b058fb61b
Volodymyr Pivtoranis <pivtoranis87@gmail.com>
2023-05-30 22:40:05 +0400

    Added restriction if not sign in

--- components/Explore.jsx
+++ components/Explore.jsx
@@ -1,10 +1,12 @@
 'use client';
 import { useState, useEffect } from 'react';
+import { useSession } from 'next-auth/react';
 import TripList from './TripList';
 import Search from './Search';
 
 const Explore = () => {
   const [trips, setTrips] = useState([]);
+  const { data: session } = useSession();
 
   useEffect(() => {
     const fetchTrips = async () => {
@@ -18,8 +20,18 @@ const Explore = () => {
   console.log(trips);
   return (
     <section className="mt-16 mx-auto w-full max-w-7xl flex justify-center items-center flex-col gap-2">
-      <Search />
-      <TripList data={trips} />
+      {session ? (
+        <>
+          <Search />
+          <TripList data={trips} />
+        </>
+      ) : (
+        <div className="rounded-xl border border-gray-200 bg-sky-300/10 shadow-inner p-5">
+          <p className="text-lg text-gray-500 sm:text-xl lg:text-2xl max-w-2xl text-center">
+            To Explore you need to Sign In
+          </p>
+        </div>
+      )}
     </section>
   );
 };
